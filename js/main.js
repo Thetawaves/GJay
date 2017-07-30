4e0f4d0ee6967fb000a767928fdd46d413dbc831
Thetawaves <jessica.granziera@gmail.com>
2017-07-30 21:51:45 +0200

    Added Portfolio Section

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 $(document).ready(function () {
-    var l=$('.diamond').width();
-    var lMini=$('#diamonds-wrap .mini').width();
+    var l = $('.diamond').width();
+    var lMini = $('#diamonds-wrap .mini').width();
     $('.diamond').css({height:l});
     $('#diamonds-wrap .mini').css({height:lMini});
+    $(window).resize(function () {
+        l = $('.diamond').width();
+        lMini = $('#diamonds-wrap .mini').width();
+        $('.diamond').css({height:l});
+        $('#diamonds-wrap .mini').css({height:lMini});
+    });
     
     $(window).on('scroll', function () {
         var scrollo = $(window).scrollTop();
-            console.log(scrollo)
+        if (scrollo > 10) {
+            $('#scroll').fadeOut()
+        } else {
+            $('#scroll').fadeIn()
+        }
     });
 })
